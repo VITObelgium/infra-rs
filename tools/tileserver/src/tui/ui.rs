b119bd95ca2b83db86b14a2955329fdd803d3821
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2024-09-20 16:59:29 +0200

    Fix invalid layer index access

--- tools/tileserver/src/tui/ui.rs
+++ tools/tileserver/src/tui/ui.rs
@@ -9,23 +9,26 @@ use tui_logger::{TuiLoggerLevelOutput, TuiLoggerWidget};
 
 use super::app::App;
 
-fn layer_metadata_rows(layer: &LayerMetadata) -> Vec<Row> {
-    vec![
-        Row::new(vec!["Id".to_string(), layer.id.to_string()]),
-        Row::new(vec!["Name".to_string(), layer.name.clone()]),
-        Row::new(vec!["EPSG".to_string(), layer.epsg.to_string()]),
-        Row::new(vec!["Format".to_string(), format!("{:?}", layer.source_format)]),
-        Row::new(vec!["Tile format".to_string(), format!("{}", layer.tile_format)]),
-        Row::new(vec!["Nodata".to_string(), format!("{:?}", layer.nodata::<f64>())]),
-        Row::new(vec!["Data type".to_string(), format!("{:?}", layer.data_type)]),
-        Row::new(vec!["Min value".to_string(), layer.min_value.to_string()]),
-        Row::new(vec!["Max value".to_string(), layer.max_value.to_string()]),
-        Row::new(vec!["Min zoom".to_string(), layer.min_zoom.to_string()]),
-        Row::new(vec!["Max zoom".to_string(), layer.max_zoom.to_string()]),
-        Row::new(vec!["Bounds".to_string(), format!("{:?}", layer.bounds)]),
-        Row::new(vec!["Tile scheme".to_string(), layer.scheme.clone()]),
-        Row::new(vec!["Tile URL".to_string(), layer.url("http://localhost:8080")]),
-    ]
+fn layer_metadata_rows(layer: Option<&LayerMetadata>) -> Vec<Row> {
+    match layer {
+        Some(layer) => vec![
+            Row::new(vec!["Id".to_string(), layer.id.to_string()]),
+            Row::new(vec!["Name".to_string(), layer.name.clone()]),
+            Row::new(vec!["EPSG".to_string(), layer.epsg.to_string()]),
+            Row::new(vec!["Format".to_string(), format!("{:?}", layer.source_format)]),
+            Row::new(vec!["Tile format".to_string(), format!("{}", layer.tile_format)]),
+            Row::new(vec!["Nodata".to_string(), format!("{:?}", layer.nodata::<f64>())]),
+            Row::new(vec!["Data type".to_string(), format!("{:?}", layer.data_type)]),
+            Row::new(vec!["Min value".to_string(), layer.min_value.to_string()]),
+            Row::new(vec!["Max value".to_string(), layer.max_value.to_string()]),
+            Row::new(vec!["Min zoom".to_string(), layer.min_zoom.to_string()]),
+            Row::new(vec!["Max zoom".to_string(), layer.max_zoom.to_string()]),
+            Row::new(vec!["Bounds".to_string(), format!("{:?}", layer.bounds)]),
+            Row::new(vec!["Tile scheme".to_string(), layer.scheme.clone()]),
+            Row::new(vec!["Tile URL".to_string(), layer.url("http://localhost:8080")]),
+        ],
+        None => Vec::new(),
+    }
 }
 
 /// Renders the user interface widgets.
@@ -55,7 +58,7 @@ pub fn render(app: &mut App, frame: &mut Frame) {
     let widths = [Constraint::Length(5), Constraint::Fill(80), Constraint::Length(10)];
 
     let meta_rows = match app.layer_table_state.selected() {
-        Some(index) => layer_metadata_rows(&app.layers[index]),
+        Some(index) => layer_metadata_rows(app.layers.get(index)),
         None => Vec::new(),
     };
 
