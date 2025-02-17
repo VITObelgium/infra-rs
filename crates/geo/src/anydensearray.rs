2f328a47cd6fdf88b3923cdf73e575972a7bdeb4
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-02-17 22:22:43 +0100

    raw_data_u8_slice

--- crates/geo/src/anydensearray.rs
+++ crates/geo/src/anydensearray.rs
@@ -173,6 +173,41 @@ impl<Metadata: ArrayMetadata> AnyDenseArray<Metadata> {
             AnyDenseArray::F64(raster) => raster.write(path),
         }
     }
+
+    /// # Safety
+    /// Used for FFI with python where safety is not guaranteed anyway
+    pub unsafe fn raw_data_u8_slice(&self) -> &[u8] {
+        match self {
+            AnyDenseArray::U8(raster) => raster.as_slice(),
+            AnyDenseArray::U16(raster) => {
+                std::slice::from_raw_parts(raster.as_slice().as_ptr().cast::<u8>(), std::mem::size_of_val(raster.as_slice()))
+            }
+            AnyDenseArray::U32(raster) => {
+                std::slice::from_raw_parts(raster.as_slice().as_ptr().cast::<u8>(), std::mem::size_of_val(raster.as_slice()))
+            }
+            AnyDenseArray::U64(raster) => {
+                std::slice::from_raw_parts(raster.as_slice().as_ptr().cast::<u8>(), std::mem::size_of_val(raster.as_slice()))
+            }
+            AnyDenseArray::I8(raster) => {
+                std::slice::from_raw_parts(raster.as_slice().as_ptr().cast::<u8>(), std::mem::size_of_val(raster.as_slice()))
+            }
+            AnyDenseArray::I16(raster) => {
+                std::slice::from_raw_parts(raster.as_slice().as_ptr().cast::<u8>(), std::mem::size_of_val(raster.as_slice()))
+            }
+            AnyDenseArray::I32(raster) => {
+                std::slice::from_raw_parts(raster.as_slice().as_ptr().cast::<u8>(), std::mem::size_of_val(raster.as_slice()))
+            }
+            AnyDenseArray::I64(raster) => {
+                std::slice::from_raw_parts(raster.as_slice().as_ptr().cast::<u8>(), std::mem::size_of_val(raster.as_slice()))
+            }
+            AnyDenseArray::F32(raster) => {
+                std::slice::from_raw_parts(raster.as_slice().as_ptr().cast::<u8>(), std::mem::size_of_val(raster.as_slice()))
+            }
+            AnyDenseArray::F64(raster) => {
+                std::slice::from_raw_parts(raster.as_slice().as_ptr().cast::<u8>(), std::mem::size_of_val(raster.as_slice()))
+            }
+        }
+    }
 }
 
 #[cfg(feature = "gdal")]
