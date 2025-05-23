585cb5e9ca8754163eb2381fe339c0df418b5772
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-05-23 20:45:28 +0200

    Unit test for tile reassembly

--- crates/raster-tile/src/utils.rs
+++ crates/raster-tile/src/utils.rs
@@ -1,7 +1,6 @@
 use crate::{Error, Result};
 use geo::{
-    Array as _, ArrayNum, Cell, CellSize, Columns, CoordinateTransformer, DenseArray, GeoReference, LatLonBounds, Rows, Tile, crs,
-    tileutils,
+    Array as _, ArrayNum, Cell, CellSize, Columns, DenseArray, GeoReference, LatLonBounds, Rows, Tile, crs, raster::DenseRaster, tileutils,
 };
 use inf::progressinfo::ProgressNotification;
 
@@ -11,20 +10,18 @@ pub fn reassemble_raster_from_tiles<T: ArrayNum>(
     tile_size: u16,
     progress: impl ProgressNotification,
     tile_cb: impl Fn(Tile) -> Result<DenseArray<T>>,
-) -> Result<DenseArray<T, GeoReference>> {
+) -> Result<DenseRaster<T>> {
     let tiles = tileutils::tiles_for_bounds(bounds, zoom);
     let top_left_tile = Tile::for_coordinate(bounds.northwest(), zoom);
     let lower_left_tile = Tile::for_coordinate(bounds.southwest(), zoom);
 
     let raster_size = tileutils::raster_size_for_tiles_containing_bounds(bounds, zoom, tile_size)?;
-
-    let trans = CoordinateTransformer::from_epsg(crs::epsg::WGS84, crs::epsg::WGS84_WEB_MERCATOR)?;
-    let lower_left = trans.transform_coordinate(lower_left_tile.bounds().southwest())?;
+    let lower_left = crs::lat_lon_to_web_mercator(lower_left_tile.bounds().southwest());
 
     let geo_ref = GeoReference::with_origin(
         crs::epsg::WGS84_WEB_MERCATOR.to_string(),
         raster_size,
-        lower_left.into(),
+        lower_left,
         CellSize::square(Tile::pixel_size_at_zoom_level(zoom)),
         Some(T::nodata_value()),
     );
@@ -65,4 +62,38 @@ pub fn reassemble_raster_from_tiles<T: ArrayNum>(
     }
 
     Ok(raster)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use geo::{
+        Coordinate,
+        raster::{DenseRaster, RasterIO},
+    };
+    use inf::progressinfo::DummyProgress;
+    use path_macro::path;
+
+    use crate::RasterTileIO;
+
+    use super::*;
+
+    #[test]
+    fn reassemble_from_tiles() {
+        let bounds = LatLonBounds::hull(Coordinate::latlon(50.67, 2.52), Coordinate::latlon(51.50, 5.91));
+
+        let test_data_dir = path!(env!("CARGO_MANIFEST_DIR") / ".." / ".." / "tests" / "data");
+
+        let raster = reassemble_raster_from_tiles(bounds, 7, 256, DummyProgress, |tile| {
+            let path = test_data_dir.join(format!("tiles/{}_{}_{}.vrt", tile.z, tile.x, tile.y));
+            assert!(path.exists(), "Tile file does not exist: {}", path.display());
+
+            let bytes = std::fs::read(&path).unwrap();
+            Ok(DenseArray::<u8>::from_tile_bytes(&bytes).unwrap().cast_to::<f32>())
+        })
+        .unwrap();
+
+        let expected = DenseRaster::<f32>::read(&test_data_dir.join("reference/reassembled_from_tiles.tif")).unwrap();
+        assert_eq!(expected, raster);
+    }
 }
