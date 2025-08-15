3146f1e3427f8848dc8fe75483a51ab0adc8aaaa
Dirk Vanden Boer <dirk.vdb@gmail.com>
2025-08-15 22:00:01 +0200

    Add test comparing proj and proj4rs transformers

--- crates/geo/src/coordinatetransformer.rs
+++ crates/geo/src/coordinatetransformer.rs
@@ -8,3 +8,28 @@ mod proj4rs;
 #[cfg(all(feature = "proj4rs", not(feature = "proj")))]
 // proj takes precedence over proj4rs if both are enabled
 pub use proj4rs::CoordinateTransformer;
+
+#[cfg(all(feature = "proj4rs", feature = "proj"))]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{Point, Result, crs};
+    use approx::assert_relative_eq;
+
+    #[test]
+    fn compare_transformers() -> Result<()> {
+        let source_crs = crs::epsg::BELGIAN_LAMBERT72;
+        let target_crs = crs::epsg::WGS84_WEB_MERCATOR;
+
+        let proj_transformer = proj::CoordinateTransformer::from_epsg(source_crs, target_crs)?;
+        let proj4rs_transformer = proj4rs::CoordinateTransformer::from_epsg(source_crs, target_crs)?;
+
+        let point = Point::new(22000.0, 245000.0);
+        let proj_result = proj_transformer.transform_point(point)?;
+        let proj4rs_result = proj4rs_transformer.transform_point(point)?;
+
+        assert_relative_eq!(proj_result, proj4rs_result, epsilon = 1e-3);
+
+        Ok(())
+    }
+}
