575907e1ef6a169d476ee048f3144403ce39b9be
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-09-16 20:44:03 +0200

    Integrate logging with COG progress output

--- tools/createcog/src/main.rs
+++ tools/createcog/src/main.rs
@@ -1,9 +1,12 @@
-use std::io::IsTerminal;
-use std::path::PathBuf;
+use std::{
+    io::{self, IsTerminal, Write},
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
 
 use clap::Parser;
 use createtiles::TileCreationOptions;
-use env_logger::{Env, TimestampPrecision};
+use env_logger::{Env, Target, TimestampPrecision};
 use kdam::{BarExt, Column, RichProgress, tqdm};
 
 use crate::createtiles::{ZoomLevelSelection, create_cog_tiles, print_gdal_translate_command};
@@ -11,6 +14,31 @@ use crate::createtiles::{ZoomLevelSelection, create_cog_tiles, print_gdal_transl
 pub type Result<T> = anyhow::Result<T>;
 
 mod createtiles;
+
+struct ProgressWriter {
+    progress: Arc<Mutex<RichProgress>>,
+}
+
+impl Write for ProgressWriter {
+    fn write(&mut self, buffer: &[u8]) -> io::Result<usize> {
+        let message = String::from_utf8_lossy(buffer);
+        let message = message.trim_end_matches(['\r', '\n']);
+
+        if !message.is_empty() {
+            self.progress
+                .lock()
+                .map_err(|_| io::Error::other("progress bar lock poisoned"))?
+                .write(message)
+                .map_err(io::Error::other)?;
+        }
+
+        Ok(buffer.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
 
 #[derive(Parser, Debug)]
 #[clap(name = "createcog", about = "Create Cloud Optimized GeoTIFF")]
@@ -59,9 +87,28 @@ fn main() -> Result<()> {
 
     kdam::term::init(std::io::stderr().is_terminal());
 
-    env_logger::Builder::from_env(Env::default().default_filter_or("warn"))
-        .format_timestamp(Some(TimestampPrecision::Millis))
-        .init();
+    let show_progress = !opt.no_progress && !opt.print_command;
+    let progress = Arc::new(Mutex::new(RichProgress::new(
+        tqdm!(total = 100, disable = !show_progress),
+        vec![
+            Column::Text("[bold blue]Creating COG".to_owned()),
+            Column::Animation,
+            Column::Percentage(1),
+            Column::Text("•".to_owned()),
+            Column::ElapsedTime,
+            Column::Text("•".to_owned()),
+            Column::RemainingTime,
+        ],
+    )));
+
+    let mut logger = env_logger::Builder::from_env(Env::default().default_filter_or("warn"));
+    logger.format_timestamp(Some(TimestampPrecision::Millis));
+    if show_progress {
+        logger.target(Target::Pipe(Box::new(ProgressWriter {
+            progress: Arc::clone(&progress),
+        })));
+    }
+    logger.init();
 
     let gdal_config = geo::RuntimeConfiguration::builder()
         .config_options(vec![
@@ -85,28 +132,21 @@ fn main() -> Result<()> {
     if opt.print_command {
         print_gdal_translate_command(&PathBuf::from(opt.input), tile_opts)?;
     } else {
-        let mut pb = RichProgress::new(
-            tqdm!(total = 100, disable = opt.no_progress),
-            vec![
-                Column::Text("[bold blue]Creating COG".to_owned()),
-                Column::Animation,
-                Column::Percentage(1),
-                Column::Text("•".to_owned()),
-                Column::ElapsedTime,
-                Column::Text("•".to_owned()),
-                Column::RemainingTime,
-            ],
-        );
-        let progress: Option<&mut dyn FnMut(f64)> = if opt.no_progress {
-            None
-        } else {
-            Some(&mut |fraction: f64| {
-                let _ = pb.update_to((fraction * 100.0) as usize);
-            })
+        let mut update_progress = |fraction: f64| {
+            if let Ok(mut progress) = progress.lock() {
+                let _ = progress.update_to((fraction * 100.0) as usize);
+            }
         };
-        create_cog_tiles(&opt.input, opt.output, tile_opts, progress)?;
-        pb.update_to(100)?;
-        eprintln!();
+        let progress_callback: Option<&mut dyn FnMut(f64)> = show_progress.then_some(&mut update_progress);
+
+        create_cog_tiles(&opt.input, opt.output, tile_opts, progress_callback)?;
+        progress
+            .lock()
+            .map_err(|_| anyhow::anyhow!("progress bar lock poisoned"))?
+            .update_to(100)?;
+        if show_progress {
+            eprintln!();
+        }
     }
 
     Ok(())
